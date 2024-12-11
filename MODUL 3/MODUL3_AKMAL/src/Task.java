2f191692740d0c0e7ef9b2402a956a505d2e011b
Ahmad Akmal Amran <ranaeza21@gmail.com>
2024-12-11 15:40:33 +0700

    MODUL3_AKMAL

---
+++ MODUL 3/MODUL3_AKMAL/src/Task.java
@@ -0,0 +1,46 @@
+import java.time.LocalDate;
+
+public class Task {
+    // TO DO: Lengkapi atribut-atribut private kelas Task
+    private String title;
+
+    private String priority;
+    private String status;
+    private final LocalDate dueDate;
+
+
+
+    // TO DO: Buat constructor untuk kelas Task
+    public Task(String title, String priority, LocalDate dueDate, String status ) {
+        this.title = title;
+        this.priority = priority;
+        this.status = status;
+        this.dueDate = dueDate;
+    }
+
+    // TO DO: Buat getter untuk title
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    // TO DO: Buat getter untuk priority
+    public String getPriority() {
+        return priority;
+    }
+    // TO DO: Buat getter untuk dueDate
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+    // TO DO: Buat getter untuk status
+    public String getStatus() {
+        return status;
+    }
+
+    // TO DO: Buat setter untuk status
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
